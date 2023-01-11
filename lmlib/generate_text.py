302b714928a704d15edac8f6d03d5c4c4762ed78
KateHajash <kshajash@gmail.com>
2023-01-11 00:03:07 -0800

    Add sampling with temperature for generation

--- lmlib/generate_text.py
+++ lmlib/generate_text.py
@@ -20,13 +20,17 @@ def generate_sequence(model: nn.Module, dataset, device, prompt: str, max_len: i
 
         for i in range(max_len):
             
+            print(sequence.shape)
             # make predictions with prompt [seq_len, 1]
             src_mask = generate_square_subsequent_mask(sequence.size(0)).to(device)
             pred = model(sequence, src_mask)
 
             # select next word using method
             # pred [seq_len, batch_size, n_tokens]
-            next_token = greedy(pred)
+            if method == "greedy":
+                next_token = greedy(pred)
+            else:
+                next_token = sampling(pred)
 
             # concatenate onto prompt and send through again
             sequence = torch.concat([sequence, torch.tensor([[next_token]], dtype=torch.long).to(device)])
@@ -37,6 +41,11 @@ def generate_sequence(model: nn.Module, dataset, device, prompt: str, max_len: i
 def greedy(pred):
     return torch.argmax(pred[-1, 0, :])
 
+def sampling(pred, temp=1):
+    scaled_probs = nn.functional.softmax(pred[-1, 0, :]) / temp
+    return torch.multinomial(scaled_probs, num_samples=1)
+
+# TODO: add more selection methods
 
 def main():
     model_path = "/home/kate/Code/language-models/lmlib/wandb/run-20230108_154613-2mnjpfp4/files/model.pt"
@@ -57,7 +66,8 @@ def main():
     model.load_state_dict(checkpoint)
 
     # Generate new sequence
-    sequence = generate_sequence(model, dataset, "How to put up a tent. In order to assemble a tent", max_len=5, method="greedy")
+    sentence = "How to put up a tent. In order to assemble a tent"
+    sequence = generate_sequence(model, dataset, device, sentence, max_len=5, method="sampling")
     # print(sequence.shape, sequence)
     print(dataset.tokens2string(sequence.cpu().numpy()))
 
